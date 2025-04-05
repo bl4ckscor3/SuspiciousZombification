5c1f70ccaf25980dc5042c1d3b7f7cabb2687ae4
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-04-05 12:01:33 +0200

    Added configuration screen support

--- src/main/java/suszombification/SuspiciousZombification.java
+++ src/main/java/suszombification/SuspiciousZombification.java
@@ -2,7 +2,9 @@ package suszombification;
 
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 import suszombification.registration.SZBlockEntityTypes;
 import suszombification.registration.SZBlocks;
 import suszombification.registration.SZDataComponents;
@@ -15,7 +17,8 @@ import suszombification.registration.SZRecipeSerializers;
 public class SuspiciousZombification {
 	public static final String MODID = "suszombification";
 
-	public SuspiciousZombification(IEventBus modEventBus) {
+	public SuspiciousZombification(IEventBus modEventBus, ModContainer container) {
+		container.registerConfig(ModConfig.Type.SERVER, SZConfig.SERVER_SPEC);
 		SZBlocks.BLOCKS.register(modEventBus);
 		SZBlockEntityTypes.BLOCK_ENTITY_TYPES.register(modEventBus);
 		SZCreativeModeTabs.CREATIVE_MODE_TABS.register(modEventBus);
