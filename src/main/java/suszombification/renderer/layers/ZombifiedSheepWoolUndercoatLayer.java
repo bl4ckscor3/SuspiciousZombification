6e2b5f28a775444500a99d0dbdc00f58e4b25ac2
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-07-09 23:27:18 +0200

    Fix zombified sheep not having a colored undercoat

---
+++ src/main/java/suszombification/renderer/layers/ZombifiedSheepWoolUndercoatLayer.java
@@ -0,0 +1,33 @@
+package suszombification.renderer.layers;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.SheepFurModel;
+import net.minecraft.client.model.SheepModel;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.entity.state.SheepRenderState;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.DyeColor;
+import suszombification.SuspiciousZombification;
+
+public class ZombifiedSheepWoolUndercoatLayer extends RenderLayer<SheepRenderState, SheepModel> {
+	private static final ResourceLocation SHEEP_WOOL_UNDERCOAT_LOCATION = SuspiciousZombification.resLoc("textures/entity/zombified_sheep/zombified_sheep_wool_undercoat.png");
+	private final EntityModel<SheepRenderState> adultModel;
+	private final EntityModel<SheepRenderState> babyModel;
+
+	public ZombifiedSheepWoolUndercoatLayer(RenderLayerParent<SheepRenderState, SheepModel> renderer, EntityModelSet modelSet) {
+		super(renderer);
+		adultModel = new SheepFurModel(modelSet.bakeLayer(ModelLayers.SHEEP_WOOL_UNDERCOAT));
+		babyModel = new SheepFurModel(modelSet.bakeLayer(ModelLayers.SHEEP_BABY_WOOL_UNDERCOAT));
+	}
+
+	public void render(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, SheepRenderState renderState, float yRot, float xRot) {
+		if (!renderState.isInvisible && (renderState.isJebSheep() || renderState.woolColor != DyeColor.WHITE))
+			coloredCutoutModelCopyLayerRender(renderState.isBaby ? babyModel : adultModel, SHEEP_WOOL_UNDERCOAT_LOCATION, poseStack, bufferSource, packedLight, renderState, renderState.getWoolColor());
+	}
+}
